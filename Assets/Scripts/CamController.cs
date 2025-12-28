1ba72f58d039e9ca991d97d2978021bb5d4ea0d4
Arda Anıl Morkoç <ardamorkoc06@gmail.com>
2025-12-28 12:24:40 +0300

    Kamera bakış açısı ayarları yapıldı

--- Assets/Scripts/CamController.cs
+++ Assets/Scripts/CamController.cs
@@ -8,17 +8,23 @@ public class CamController : MonoBehaviour
     [SerializeField] Transform playerBody;
     [SerializeField] Joystick lookJoystick;
 
+    [Header("Camera Limits")]
+    [SerializeField] float minY = -35f; // aşağı bakma
+    [SerializeField] float maxY = 40f;  // yukarı bakma
+
     float xRotation = 0f;
 
-    private void Update()
+    void Update()
     {
         float mouseX = lookJoystick.Horizontal * mouseSensitivity * Time.deltaTime;
         float mouseY = lookJoystick.Vertical * mouseSensitivity * Time.deltaTime;
 
+        // YUKARI - AŞAĞI (KAMERA)
         xRotation -= mouseY;
-
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, minY, maxY);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // SAĞ - SOL (PLAYER)
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
